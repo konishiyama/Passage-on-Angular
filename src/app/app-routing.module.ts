c590ba9dd57bfe831984e1a0318dbd60cc3f3a7b
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2020-05-06 16:25:34 +0900

    front_end_1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToppageComponent } from './toppage/toppage.component';
+import { AboutComponent } from './about/about.component';
+import { ArticleIndexComponent } from './article-index/article-index.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ToppageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'article_index', component: ArticleIndexComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'careers', component: CareersComponent },
+  { path: 'contact_us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ToppageComponent } from './toppage/toppage.component';
+import { ArticleIndexComponent } from './article-index/article-index.component';
+import { CareersComponent } from './careers/careers.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ToppageComponent,
+    ArticleIndexComponent,
+    CareersComponent,
+    ArticlesComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
